cc22fddeae9e46f1e06a4452579e98b41e11d7b1
Mayur Rathod <mdridrdrr@gmail.com>
2024-01-16 23:10:17 +0530

    Used body parser as middleare to get body of request

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const bodyparser = require("body-parser")
 
 const app = express()
 const port = 8080
@@ -9,7 +10,8 @@ const middleware = (req,res,next)=>{
     next()
 }
 
-app.use(middleware)
+// app.use(middleware)
+app.use(bodyparser.json())
 const fib = (n)=>{
     let a = 0, b = 1, c, i;
     if( n == 0)
@@ -26,19 +28,20 @@ const postCr = (req,res)=>{
     res.send("This is post")
 }
 app.post('/handle',(req,res)=>{
-    let a = req.headers.counter
-    console.log(req.headers)
+    // let a = req.headers.counter using header
+    // let a = req.query.counter with query ?counter=10
+    console.log(req.body)
     console.log("Gettin")
- 
+    let a = req.body.counter 
     res.send("fibs "+fib(a))
 })
-app.post('/posting',postCr)
-app.put('/putt',(req,res)=>{
-    res.json({'a':'Mayur'})
-})
-app.delete('/delete',(res,rep)=>{
-    rep.send("Testing Delete")
-})
+// app.post('/posting',postCr)
+// app.put('/putt',(req,res)=>{
+//     res.json({'a':'Mayur'})
+// })
+// app.delete('/delete',(res,rep)=>{
+//     rep.send("Testing Delete")
+// })
 app.listen(port,()=>{
     console.log("Server connected")
 })
